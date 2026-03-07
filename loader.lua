1b82090dc413ef6695ef95d7ffcfa5d5763bd5e8
azurion <durkmcteam@gmail.com>
2026-03-07 16:44:11 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -47,6 +47,8 @@ local scripts = {
     [131623223084840] = "https://raw.githubusercontent.com/azuriondeve/azurionhub/refs/heads/main/games/Wave%20a%20Brainrot/tsunami.lua",
     [126509999114328] = "https://raw.githubusercontent.com/azuriondeve/azurionhub/refs/heads/main/games/99%20Nights/main.lua",
     [994732206] = "https://raw.githubusercontent.com/azuriondeve/azurionhub/refs/heads/main/games/bloxfruit/main.lua", -- Exemplo de GameId
+    [104901094326217] = "https://raw.githubusercontent.com/azuriondeve/azurionhub/refs/heads/main/games/engordaecaia/main.lua",
+
 }
 
 -- Theme
